44edf1cba3385a14f2c608f14af8a7687ebd5fd4
burster11 <55365064+burster11@users.noreply.github.com>
2021-03-12 15:59:40 -0500

    Add files via upload

---
+++ Week 7/src/components/Disclaimer.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+class Disclaimer extends React.Component {
+    render () {
+        return <h2 style= {{
+            fontsize: "35px",
+            color: "#41a3db",
+        }}>Always warm up before participating in physical activity.</h2>
+    }
+}
+export default Disclaimer;
